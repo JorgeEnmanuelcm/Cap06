5b80f3fd0c9878393df253c3eff3c862a5f1cc23
Jorge Enmanuel Castro Mendez <georgeecm@outlook.com>
2015-09-21 22:58:14 +0200

    Realizando arreglo jagged

--- ProjectCap06/Form1.Designer.cs
+++ ProjectCap06/Form1.Designer.cs
@@ -28,12 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonCalcular = new System.Windows.Forms.Button();
+            this.textBoxRes = new System.Windows.Forms.TextBox();
+            this.listBoxList = new System.Windows.Forms.ListBox();
+            this.labelLista = new System.Windows.Forms.Label();
+            this.labelPromedio = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonCalcular
+            // 
+            this.buttonCalcular.Location = new System.Drawing.Point(96, 200);
+            this.buttonCalcular.Name = "buttonCalcular";
+            this.buttonCalcular.Size = new System.Drawing.Size(63, 23);
+            this.buttonCalcular.TabIndex = 0;
+            this.buttonCalcular.Text = "Calcular";
+            this.buttonCalcular.UseVisualStyleBackColor = true;
+            this.buttonCalcular.Click += new System.EventHandler(this.buttonCalcular_Click);
+            // 
+            // textBoxRes
+            // 
+            this.textBoxRes.Location = new System.Drawing.Point(96, 229);
+            this.textBoxRes.Name = "textBoxRes";
+            this.textBoxRes.Size = new System.Drawing.Size(63, 20);
+            this.textBoxRes.TabIndex = 2;
+            this.textBoxRes.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // listBoxList
+            // 
+            this.listBoxList.FormattingEnabled = true;
+            this.listBoxList.Items.AddRange(new object[] {
+            "7",
+            "6",
+            "1",
+            "2",
+            "9",
+            "3",
+            "5",
+            "8"});
+            this.listBoxList.Location = new System.Drawing.Point(39, 70);
+            this.listBoxList.Name = "listBoxList";
+            this.listBoxList.Size = new System.Drawing.Size(120, 108);
+            this.listBoxList.TabIndex = 3;
+            this.listBoxList.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // labelLista
+            // 
+            this.labelLista.AutoSize = true;
+            this.labelLista.Location = new System.Drawing.Point(39, 27);
+            this.labelLista.Name = "labelLista";
+            this.labelLista.Size = new System.Drawing.Size(87, 13);
+            this.labelLista.TabIndex = 4;
+            this.labelLista.Text = "Lista de numeros";
+            // 
+            // labelPromedio
+            // 
+            this.labelPromedio.AutoSize = true;
+            this.labelPromedio.Location = new System.Drawing.Point(36, 229);
+            this.labelPromedio.Name = "labelPromedio";
+            this.labelPromedio.Size = new System.Drawing.Size(54, 13);
+            this.labelPromedio.TabIndex = 5;
+            this.labelPromedio.Text = "Promedio:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.labelPromedio);
+            this.Controls.Add(this.labelLista);
+            this.Controls.Add(this.listBoxList);
+            this.Controls.Add(this.textBoxRes);
+            this.Controls.Add(this.buttonCalcular);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonCalcular;
+        private System.Windows.Forms.TextBox textBoxRes;
+        private System.Windows.Forms.ListBox listBoxList;
+        private System.Windows.Forms.Label labelLista;
+        private System.Windows.Forms.Label labelPromedio;
     }
 }
 
--- ProjectCap06/Form1.cs
+++ ProjectCap06/Form1.cs
@@ -16,5 +16,40 @@ namespace ProjectCap06
         {
             InitializeComponent();
         }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+           
+
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int[][] valores = new int[3][];
+            
+            valores[0] = new int[] { 7, 6, 1 };
+            valores[1] = new int[] { 2, 9 };
+            valores[2] = new int[] { 3, 5, 8 };
+
+        }
+
+        private void buttonCalcular_Click(object sender, EventArgs e)
+        {
+
+            int Resultado=int.Parse(textBoxLados.Text)
+            textBoxRes.Text = Resultado.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
+
